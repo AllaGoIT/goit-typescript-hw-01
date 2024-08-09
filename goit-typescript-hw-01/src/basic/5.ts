ff9b03bf90c36d2a9c5a42ece96b403a10a6d297
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-08-09 16:44:44 +0300

    add code finish task1

--- goit-typescript-hw-01/src/basic/5.ts
+++ goit-typescript-hw-01/src/basic/5.ts
@@ -1,30 +1,53 @@
-// enum class DayOfWeek {
-//   Monday = false;
+// enum  DayOfWeek {
+//   Monday= false;
 //   Tuesday = false;
 //   Wensday = false;
-//   Thurday = false;
+//   Thursday = false;
 //   Friday = false;
 //   Saturday = true;
 //   Sunday = true;
 // }
 
-// const isWeekend = (day: DayOfWeek) => {};
+// const isWeekend = (day: DayOfWeek): boolean => {
+//   return DayOfWeek.FRIDAY;
+// };
 
 // isWeekend(DayOfWeek.Friday);
+
+// enum DayOfWeek {
+//   Monday = "false",
+//   TUESDAY = "false",
+//   WENDSDAY = "false",
+//   Thursday = "false",
+//   FRIDAY = "false",
+//   SATURDAY = "true",
+//   SUNDAY = "true",
+// }
+
+// const isWeekend = (day: DayOfWeek) => {
+//   return DayOfWeek.FRIDAY;
+// };
+
+// isWeekend(DayOfWeek.FRIDAY);
+
+// Типізуйте функцію isWeekend яка приймає день
+// тижня з enumDayOfWeek і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 
 enum DayOfWeek {
   MONDAY = "false",
   TUESDAY = "false",
   WENDSDAY = "false",
-  Thurday = "false",
+  ThURSDAY = "false",
   FRIDAY = "false",
   SATURDAY = "true",
   SUNDAY = "true",
 }
 
-const isWeekend = (day: DayOfWeek) => {};
-
+const isWeekend = (day: DayOfWeek) => {
+  if (DayOfWeek.FRIDAY === "false") {
+    return false;
+  } else {
+    return true;
+  }
+};
 isWeekend(DayOfWeek.FRIDAY);
-
-// Типізуйте функцію isWeekend яка приймає день
-// тижня з enumDayOfWeek і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
