26674a6c0c26760bf354588b9b51e87e50ee83a9
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-08-10 22:39:05 +0300

    add code task 2

--- goit-typescript-hw-01/src/generics/1.ts
+++ goit-typescript-hw-01/src/generics/1.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-async function fetchData<T>(url: string): Promise:<T> {
+async function fetchData<T>(url: string): Promise<T> {
   try {
-    const response = await axios.get("url");
+    const response = await axios.get(url);
     return response.data;
   } catch (error) {
-    throw new Error(`Error fetching from ${"url"}: ${error}`);
+    throw new Error(`Error fetching from ${url}: ${error}`);
   }
 }
 
